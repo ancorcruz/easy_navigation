735b9789ac3b8ed313749a56fd35a39727b88b91
Ancor Cruz <contactar@ancorcruz.com>
2009-03-30 16:09:37 +0100

    Added recursive depth menus Added option for define depth of required menu Added Tab's clones (define other accions with this tab active) Remove Menu Class Use role_requirement for check tabs users permissions

--- lib/easy_navigation.rb
+++ lib/easy_navigation.rb
@@ -3,74 +3,93 @@ module EasyNavigation
   module Helper
   
     def easy_navigation(name, options = {})
-
       navigation_class_name = (options[:navigation_class] || "navigation").to_s
       tab_class_name = (options[:tab_class] || "tab").to_s
-      menu_class_name = (options[:menu_class] || "menu").to_s
       separator_class_name = (options[:separator_class] || "separator").to_s
       separator = options[:separator] || false
+      deep_level = options[:deep_level] || 0
 
-      tabs_html = ""
-      
-      EasyNavigation::Builder.navigation[name][:tabs].each do |tab|
-        menus_html = ""
-        current_tab = false
-        tab[:menus].each do |menu|
-          if current = current_menu?(menu)
-            current_tab = true
-          end
-          menus_html << self.render_menu(menu[:name], menu[:text], 
-            menu[:url].merge!(:skip_relative_url_root => true), 
-            ("#{menu_class_name} current" if current) || menu_class_name)
-          if separator
-           menus_html << self.render_separator(separator_class_name)
-          end
-        end
-        tabs_html << self.render_tab(tab[:name], 
-          tab[:text], 
-          tab[:url].merge!(:skip_relative_url_root => true), 
-          menus_html, ("#{tab_class_name} current" if current_tab) || tab_class_name)
-      end
+      tabs_html = render_tabs(EasyNavigation::Builder.navigation[name], 0, deep_level, tab_class_name)
       self.render_navigation("navigation_" << name.to_s, tabs_html, navigation_class_name)
     end
     
     protected
     
+    def render_tabs(tab, deep, deep_level, tab_class_name)
+      tabs_html = ''
+      if deep == deep_level
+        active_tab = current_tab?(tab)
+        
+        tab[:tabs].each do |subtab|
+          tab_class = tab_class_name
+          tab_class += " first" if tabs_html.empty?
+          tab_class += " active" if current_tab?(subtab)
+          tab_class = tab_class.strip
+
+          if active_tab && user_authorized_tab?(subtab)
+            tabs_html << self.render_tab(subtab[:name],
+               subtab[:text],
+               action_for_tab(subtab)[:url].merge!(:skip_relative_url_root => true),
+               tab_class)
+          end
+        end
+      else
+        tab[:tabs].each do |subtab|
+          tabs_html = render_tabs(subtab, deep + 1, deep_level, tab_class_name)
+          return tabs_html unless tabs_html.empty?
+        end
+      end
+      tabs_html
+    end
+
+    # This code could be better but works!
+    def current_tab?(tab)
+      if tab[:tabs].empty?
+        if tab[:url][:action]
+          return (controller.params[:controller] == tab[:url][:controller] &&
+                     controller.params[:action] == tab[:url][:action]) ||
+                     (tab[:clones].include? controller.params[:controller])
+        else
+          return (controller.params[:controller] == tab[:url][:controller] &&
+                     controller.params[:action] == 'index') || 
+                     (tab[:clones].include? controller.params[:controller])
+        end
+      else
+        tab[:tabs].each { |subtab| return true if current_tab?(subtab) }
+      end
+      false
+    end
+
+    def action_for_tab(tab)
+      return tab unless controller.class.methods.include?("user_authorized_for?") 
+      return tab if (tab[:url][:controller] + "_controller").camelize.constantize.user_authorized_for?(current_user, { :action => tab[:url][:action] }, binding)
+      
+      tab[:tabs].each { |subtab| return subtab if action_for_tab(subtab) } unless tab[:tabs].empty?
+      nil
+    end
+
+    # This code could be better but works!
+    def user_authorized_tab?(tab)
+      return true unless controller.class.methods.include?("user_authorized_for?") 
+      tab[:tabs].each { |subtab| return true if user_authorized_tab?(subtab) } unless tab[:tabs].empty?
+      
+      if tab[:url][:action]
+        return (tab[:url][:controller] + "_controller").camelize.constantize.user_authorized_for?(current_user, { :action => tab[:url][:action] }, binding)
+      else
+        return (tab[:url][:controller] + "_controller").camelize.constantize.user_authorized_for?(current_user, { :action => 'index' }, binding)
+      end
+    end
+
     def render_separator(class_name)
       content_tag("li", "|", :class => class_name)
     end
     
-    def render_menu(id, text, url, class_name)
+    def render_tab(id, text, url, class_name)
       content_tag("li", link_to(t(text), url), :id => id, :class => class_name)
     end
-    
-    def render_tab(id, text, url, menus_html, class_name)
-      content_tag("li",
-        "#{link_to(t(text), url)} #{content_tag("ul", menus_html)}",
-        :id => id, :class => class_name)
-    end
-    
+
     def render_navigation(id, tabs_html, class_name)
       content_tag("ul", tabs_html, :id => id, :class => class_name)
-    end
-    
-    def current_menu?(menu)
-      current = controller.params[:controller] == menu[:url][:controller].gsub(/^\//, "") && 
-        (controller.action_name == menu[:url][:action] || menu[:url][:action] == nil)
-      if menu.has_key?:on 
-         (menu[:on].is_a?(Array) ? menu[:on] : [menu[:on]]).each do |controllers|
-          (controllers.is_a?(Array) ? controllers : [controllers]).each do |c|
-             current |= controller.params[:controller] == c[:controller].gsub(/^\//, "")
-            if c.has_key?:only
-              current &= (c[:only].is_a?(Array) ? c[:only] : [c[:only]]).include?controller.action_name
-            end
-            if c.has_key?:except
-              current &= !((c[:except].is_a?(Array) ? c[:except] : [c[:except]]).include?controller.action_name)
-            end
-          end
-        end      
-      end
-      current
     end
 
   end # EasyNavigation::Helper
@@ -101,7 +120,7 @@ module EasyNavigation
         
       def navigation(name, options = {}, &block)
         navigation = Navigation.new(name, options.merge!(:prefix => "navigation"))
-        yield navigation
+        yield navigation if block
         self.navigations << navigation.build
       end
       
@@ -121,7 +140,7 @@ module EasyNavigation
       
         def tab(name, options = {}, &block)
           tab = Tab.new(name, options.merge!(:prefix => [self.options[:prefix], self.name]))
-          yield tab
+          yield tab if block
           self.tabs << tab.build
         end
         
@@ -132,52 +151,34 @@ module EasyNavigation
         
         class Tab
         
-          attr_accessor :menus, :name, :options
+          attr_accessor :tabs, :clones, :name, :options
           
           def initialize(name, options = {})
-            self.menus = []
+            self.tabs = []
+            self.clones = []
             self.name = name
             self.options = options
           end
-          
-          def menu(name, options = {}, &block)
-            menu = Menu.new(name, options.merge!(:prefix => [self.options[:prefix], self.name, "menus"]))
-            yield menu
-            self.menus << menu.build
+
+          def clone(options)
+            self.clones << options[:controller]
           end
-          
+
+          def tab(name, options = {}, &block)
+            tab = Tab.new(name, options.merge!(:prefix => [self.options[:prefix], self.name]))
+            yield tab if block
+            self.tabs << tab.build
+          end
+
           def build
             { :name => [self.options[:prefix], self.name].join("_").to_sym, 
               :text => [self.options[:prefix], self.name, "title"].join("."), 
               :url => self.options[:url],
-              :options => self.options, 
-              :menus => self.menus }
+              :options => self.options,
+              :tabs => self.tabs,
+              :clones => self.clones }
           end
-          
-          class Menu
-            
-            attr_accessor :name, :options, :active_urls
-            
-            def initialize(name, params = {})
-              self.active_urls = []
-              self.name = name
-              self.options = params
-            end
-            
-            def build
-              self.options[:prefix] << self.name
-              { :name => self.options[:prefix].join("_").to_sym, 
-                :text => self.options[:prefix].join("."), 
-                :url => self.options[:url], 
-                :on => self.active_urls }
-            end
-            
-            def connect(options = {})
-              options[:controller] = self.options[:url][:controller] unless options.has_key?(:controller)
-              self.active_urls << options
-            end
 
-          end #Menu
         end # Tab
       end # Navigation
     end # Builder
